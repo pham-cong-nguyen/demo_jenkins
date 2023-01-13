830e466124098e448c9b0d8d5bdbaa736f9f33a1
pham-cong-nguyen <Pcnguyen97@gmail.com>
2023-01-13 17:57:27 +0700

    unit test

---
+++ src/test/java/com/example/demo/UnitTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class UnitTest {
+    @org.junit.jupiter.api.Test
+    void sum() {
+        assert(5 == 6);
+    }
+}
